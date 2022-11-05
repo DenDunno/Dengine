8cc58d62f25b3df6a147ee6b75ad5b302802698d
DenDunno <60221683+DenDunno@users.noreply.github.com>
2022-11-05 19:03:24 +0200

    arrtibutes pointers merge

--- GameEngine/World/UpdateCycleFactory.cs
+++ GameEngine/World/UpdateCycleFactory.cs
@@ -17,93 +17,53 @@ public class UpdateCycleFactory
     {
         var gameObjects = new List<GameObject>()
         {
-            CreateSkybox(),
-            CreateStaticPoint(),
-            CreateCube(new Vector3(-2, 2, 0), true),
-            CreateCube(new Vector3(2, 2, 0), false),
+            CreateObstacle(true, Primitives.Quad(0.5f), new Vector3(-1.5f, 0, 0)),
+            CreateObstacle(false, Primitives.Quad(0.75f), new Vector3(1.5f, -1f, 0)),
         };
         
         var world = new World(_camera, gameObjects);
         return new UpdateCycle(_window, world, _rigidbodies);
     }
-    
-    private GameObject CreateStaticPoint()
+
+    private GameObject CreateObstacle(bool isControlling, Mesh mesh, Vector3 position)
     {
+        var transform = new Transform(position);
+        var meshWorldView = new MeshWorldView(transform, mesh);
+        var shaderProgram = new ColorShaderProgram("Shaders/vert.glsl", "Shaders/uv.glsl");
+        
+        _rigidbodies.Add(new Rigidbody(transform, meshWorldView)
+        {
+            ColorShaderProgram = shaderProgram
+        });
+        
         return new GameObject(new GameObjectData()
         {
-            Components = new IUpdatable[]
+            Model = new Model(new RenderData()
             {
-                new Timer(), 
-                new CameraControlling(_camera, _window.MouseState, _window.KeyboardState),
-                new FPSCounter(_window)
-            } 
+                Transform = transform,
+                Mesh = mesh,
+                BufferUsageHint = BufferUsageHint.StaticDraw,
+                ShaderProgram = shaderProgram,
+            }),
+            
+            Components = GetComponents(isControlling, transform)
         });
     }
 
-    private GameObject CreateCube(Vector3 position, bool isControlling)
+    private IUpdatable[] GetComponents(bool isControlling, Transform transform)
     {
-        Mesh mesh = Primitives.Cube(0.5f);
-        var lightData = new LightData(new Vector3(1, 0, 0), new Texture("Resources/crate.png"), new Vector3(-4, 3, -3));
-        var transform = new Transform(position);
-        var renderData = new RenderData()
-        {
-            Transform = transform,
-            Mesh = mesh,
-            BufferUsageHint = BufferUsageHint.StaticDraw,
-            ShaderProgram = new LightningShaderProgram(lightData, _camera, "Shaders/vert.glsl", "Shaders/lightning.glsl"),
-        };
-        
         var components = new List<IUpdatable>();
-
+        
         if (isControlling)
         {
             components.Add(new ObjectControlling(transform, _window.KeyboardState));
         }
+        
         else
         {
-            components.Add(new RotationAnimation(transform, new Vector3(0, 1, 1), 1f));
+            components.Add(new RotationAnimation(transform, new Vector3(0, 0, 1), 1f));   
         }
 
-        var meshWorldView = new MeshWorldView(transform, mesh);
-        _rigidbodies.Add(new Rigidbody(transform, meshWorldView)
-        {
-            LightningShaderProgram = (LightningShaderProgram)renderData.ShaderProgram
-        });
-
-        return new GameObject(new GameObjectData()
-        {
-            Model = new Model(renderData),
-            Components = components.ToArray()
-        });
-    }
-    
-    private GameObject CreateSkybox()
-    {
-        var transform = new Transform(new Vector3(0, 0, 0));
-        var paths = new List<string>()
-        {
-            "Resources/Storm/right.jpg",
-            "Resources/Storm/left.jpg",
-            "Resources/Storm/top.jpg",
-            "Resources/Storm/bottom.jpg",
-            "Resources/Storm/back.jpg",
-            "Resources/Storm/front.jpg",   
-        };
-        var renderData = new RenderData()
-        {
-            Transform = transform,
-            Mesh = Primitives.Cube(50f),
-            BufferUsageHint = BufferUsageHint.StaticDraw,
-            ShaderProgram = new ShaderProgramWithTexture(new Cubemap(paths), "Shaders/skyboxVert.glsl", "Shaders/skyboxFrag.glsl"),
-        };
-
-        return new GameObject(new GameObjectData()
-        {
-            Model = new Model(renderData),
-            Components = new IUpdatable[]
-            {
-                new Skybox(_camera, transform)
-            },
-        });
+        return components.ToArray();
     }
 }
