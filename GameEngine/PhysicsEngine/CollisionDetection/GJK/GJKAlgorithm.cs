99f54c4a00c1d931440b4e11fcf3d7282601b28f
DenDunno <60221683+DenDunno@users.noreply.github.com>
2022-11-22 19:07:04 +0200

    start

---
+++ GameEngine/PhysicsEngine/CollisionDetection/GJK/GJKAlgorithm.cs
@@ -0,0 +1,52 @@
+﻿using OpenTK.Mathematics;
+
+public class GJKAlgorithm : ICollisionDetection
+{
+    private readonly Vector3 _origin = Vector3.Zero;
+    private readonly Simplex _simplex = new();
+        
+    public bool CheckCollision(Rigidbody objectA, Rigidbody objectB)
+    {
+        Vector3 direction = Vector3.UnitX;
+        Vector3[] positionsA = objectA.MeshWorldView.Positions;
+        Vector3[] positionsB = objectB.MeshWorldView.Positions;
+        
+        _simplex.A = GetSupportPoint(direction, positionsA, positionsB);
+        direction = _origin - _simplex.A;
+
+        Vector3 simplexB = GetSupportPoint(direction, positionsA, positionsB);
+
+        if (Vector3.Dot(simplexB, direction) < 0)
+        {
+            return false;
+        }
+
+        _simplex.B = simplexB;
+        direction = Algorithms.GetNormal(_simplex.B - _simplex.A);
+            
+        _simplex.C = GetSupportPoint(direction, positionsA, positionsB);
+            
+        return Vector3.Dot(_origin, Algorithms.GetNormal(_simplex.B - _simplex.C)) < 0 && Vector3.Dot(_origin, Algorithms.GetNormal(_simplex.A - _simplex.C)) < 0;
+    }
+
+    private Vector3 GetSupportPoint(Vector3 direction, IReadOnlyCollection<Vector3> objectA, IReadOnlyCollection<Vector3> objectB)
+    {
+        return GetFurthestPoint(direction, objectA) - GetFurthestPoint(-direction, objectB);
+    }
+
+    private Vector3 GetFurthestPoint(Vector3 direction, IReadOnlyCollection<Vector3> positions)
+    {
+        Vector3 supportPoint = Vector3.Zero;
+        float maxDot = float.MinValue;
+        
+        foreach (Vector3 position in positions)
+        {
+            if (Vector3.Dot(position, direction) > maxDot)
+            {
+                supportPoint = position;
+            }
+        }
+
+        return supportPoint;
+    }
+}
---
+++ GameEngine/PhysicsEngine/CollisionDetection/GJK/Simplex.cs
@@ -0,0 +1,14 @@
+﻿using OpenTK.Mathematics;
+
+public class Simplex
+{
+    public Vector3 A;
+    public Vector3 B;
+    public Vector3 C;
+    private readonly Vector3 _origin = Vector3.Zero;
+
+    public bool IsOriginInside()
+    {
+        return false;
+    }
+}
