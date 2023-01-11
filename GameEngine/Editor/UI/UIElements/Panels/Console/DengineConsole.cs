7d6717ae215bf7e1097f22ea10007b25cb15005f
Denis Bondar <denisbondarbarbar@gmail.com>
2023-01-11 17:35:32 +0200

    console log time

---
+++ GameEngine/Editor/UI/UIElements/Panels/Console/DengineConsole.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using ImGuiNET;
+
+public class DengineConsole : Panel
+{
+    private static readonly List<Log> _logs = new();
+    
+    public DengineConsole() : base("Console")
+    {
+    }
+
+    public static void Log(string text)
+    {
+        _logs.Add(new Log(text));
+    }
+
+    protected override void OnPanelDraw()
+    {
+        if (ImGui.Button("Clear logs"))
+        {
+            _logs.Clear();
+        }
+        
+        foreach (Log log in _logs)
+        {
+            ImGui.TextDisabled($"{log.Time:H:mm:ss}: "); 
+            ImGui.SameLine();
+            ImGui.TextColored(new Vector4(255 / 255f, 167 / 255f, 32 / 255f, 1.00f), log.Text);
+        }
+    }
+}
---
+++ GameEngine/Editor/UI/UIElements/Panels/Console/Log.cs
@@ -0,0 +1,12 @@
+﻿
+public readonly struct Log
+{
+    public readonly string Text;
+    public readonly DateTime Time;
+
+    public Log(string text)
+    {
+        Text = text;
+        Time = DateTime.Now;
+    }
+}
